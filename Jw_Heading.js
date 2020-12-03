ed59c251bacea783d2d962b290c306bc09c21b27
Robert Trevino <roberts.galore85@gmail.com>
2020-12-02 18:40:25 -0600

    edit unfamilar wording

--- Jw_Heading.js
+++ Jw_Heading.js
@@ -1,6 +1,10 @@
-
-
-
-
-var __adobewebfontsappname__="dreamweaver"
-
+javadocument
+@media (max-width: 644px){
+}
+function MM_callJS(jsStr) { //v2.0
+  return eval(jsStr)
+}
+ function MM_setTextOfLayer(objId,x,newText) { //v9.0
+  with (document) if (getElementById && ((obj=getElementById(objId))!=null))
+    with (obj) innerHTML = unescape(newText)
+ }
